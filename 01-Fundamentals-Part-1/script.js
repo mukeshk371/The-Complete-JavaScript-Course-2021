0dd3267d058b69b6c0f4f19e22e72cb14818d792
Your_Name <email_address@domain.com>
2021-10-21 09:13:21 +0530

    values, variables & data types course completed

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,41 @@
+let js = 'amazing';
+10 + 20 + 30 - 10;
+
+// New Code
+let firstName = 'Mukesh';
+// console.log(firstName);
+
+// Wrong way of writting variable names
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+// Right way of writting variable names
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+
+/*-------------------------------------------------------------------------------------------*/
+/*                                         Data Type                                         */
+/*-------------------------------------------------------------------------------------------*/
+let javascriptFun = true;
+console.log(javascriptFun);
+
+console.log(typeof javascriptFun);
+console.log(typeof 23);
+console.log(typeof "Mukesh");
+console.log(typeof true);
+
+// Change Data Type
+javascriptFun = "Mukesh";
+console.log(typeof javascriptFun);
+
+// "undefined" Data Type
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+// It's a bug of javaScript
+console.log(typeof null);
